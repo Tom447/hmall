24f929ff23fb570d208765be06e8418bc06e5d77
xiaoma <1142322503@qq.com>
2026-01-19 23:35:27 +0800

    最佳实践之前，保存； 抽取公共api

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,9 +1,11 @@
 package com.hmall.cart;
 
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +14,8 @@ import org.springframework.web.client.RestTemplate;
 
 
 @MapperScan("com.hmall.cart.mapper")
-@EnableFeignClients
+//clients = ItemClient.class,
+@EnableFeignClients()
 @SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
