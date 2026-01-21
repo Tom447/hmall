85edb278b8ead2f0dcb4a2228763889741c8c242
xiaoma <1142322503@qq.com>
2026-01-21 17:48:47 +0800

    openfegin内微服务之间进行用户信息传递； hmall全部进行微服务拆分

--- hmall/hm-api/src/main/java/com/hmall/api/config/FeignLogLevelConfig.java
+++ hmall/hm-api/src/main/java/com/hmall/api/config/FeignLogLevelConfig.java
@@ -1,11 +1,18 @@
 package com.hmall.api.config;
 
+import com.hmall.api.interceptors.UserInfoInterceptpor;
 import feign.Logger;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class FeignLogLevelConfig {
     @Bean
     public Logger.Level feignLogLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoInterceptor() {
+        return new UserInfoInterceptpor();
     }
 }
